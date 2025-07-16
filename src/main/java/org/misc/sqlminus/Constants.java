8f32ef209950e121afc749cfaca217238dd13651
Elbin Pallimalil <elbin.p@gmail.com>
2025-07-16 16:37:30 +0530

    Save display options in preferences

--- src/main/java/org/misc/sqlminus/Constants.java
+++ src/main/java/org/misc/sqlminus/Constants.java
@@ -15,6 +15,15 @@ public class Constants {
 		public static final String WINDOW_Y = "WindowY";
 		public static final String LOOK_AND_FEEL_CLASS = "LookAndFeelClass";
 		public static final String SESSION_PREFIX = "Session-";
+		public static final String MIN_COL_WIDTH = "MinColWidth";
+		public static final String MAX_COL_WIDTH = "MaxColWidth";
+		public static final String INTER_COLUMN_SPACING = "InterColumnSpacing";
+		public static final String MAX_DATA_LENGTH = "MaxDataLength";
+		public static final String NULL_REPRESENTATION = "NullRepresentation";
+		public static final String ALWAYS_SHOW_ROW_DIVIDERS = "AlwaysShowRowDividers";
+		public static final String CLEAR_SCREEN_BEFORE_EACH_RESULT = "ClearScreenBeforeEachResult";
+		public static final String ENABLE_THREADS = "EnableThreads";
+		public static final String OUTPUT_IN_GRID = "OutputInGrid";
 	}
 
 	public static final String SQL_HISTORY_FULL_FILE_PATH = System.getProperty("user.home")
