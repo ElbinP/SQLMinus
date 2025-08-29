d5c6c27fd6a336e9fef65cec66c5050703666763
Elbin Pallimalil <elbin.p@gmail.com>
2025-08-29 22:05:30 +0530

    Refactor metadata processing

--- src/main/java/org/misc/sqlminus/DisplayResultSetAsGrid.java
+++ src/main/java/org/misc/sqlminus/DisplayResultSetAsGrid.java
@@ -55,36 +55,6 @@ public class DisplayResultSetAsGrid {
 		}
 	}
 
-	private ResultSet getMetadataResult(MetadataRequestEntity metadataRequestEntity)
-			throws SQLException, SQLMinusException {
-		if (connection.isEmpty()) {
-			throw new SQLMinusException("connection not available");
-		}
-		ResultSet rst;
-		switch (metadataRequestEntity.metadataRequestType()) {
-		case CATALOGS:
-			rst = connection.get().getMetaData().getCatalogs();
-			break;
-		case SCHEMAS:
-			rst = connection.get().getMetaData().getSchemas(metadataRequestEntity.catalogName(),
-					metadataRequestEntity.schemaPattern());
-			break;
-		case TABLES:
-			rst = connection.get().getMetaData().getTables(metadataRequestEntity.catalogName(),
-					metadataRequestEntity.schemaPattern(), metadataRequestEntity.tablePattern(), null);
-			break;
-		case COLUMNS:
-			rst = connection.get().getMetaData().getColumns(metadataRequestEntity.catalogName(),
-					metadataRequestEntity.schemaPattern(), metadataRequestEntity.tablePattern(),
-					metadataRequestEntity.columnPattern());
-			break;
-		default:
-			throw new SQLMinusException("Unknown metadata request type " + metadataRequestEntity.metadataRequestType());
-		}
-
-		return rst;
-	}
-
 	public void run() {
 		try {
 			ResultSet rst = null;
@@ -105,7 +75,7 @@ public class DisplayResultSetAsGrid {
 				} else if (resultSet.isPresent()) {
 					rst = resultSet.get();
 				} else if (metadataRequestEntity.isPresent()) {
-					rst = getMetadataResult(metadataRequestEntity.get());
+					rst = DisplayResultSetUtil.getMetadataResult(metadataRequestEntity.get(), connection);
 				} else {
 					throw new SQLMinusException("None of execution command, result set and metadata request provided");
 				}
